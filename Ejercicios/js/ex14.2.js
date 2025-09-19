5dde2b260b91f9f8e1df10f7cc6acc74364e74c6
vjp-yusefLC <ylaroussid01@informatica.iesvalledeljerteplasencia.es>
2025-09-19 16:18:03 +0200

    ex14.2

---
+++ Ejercicios/js/ex14.2.js
@@ -0,0 +1,5 @@
+let array = [1,2,"a","d","g"];
+
+array.splice(2,2,"pepe",3,"H");
+console.log(array);
+// Posición inicial para empezar a eliminar elementos, número de elementos a añadir y elementos para añadir.
