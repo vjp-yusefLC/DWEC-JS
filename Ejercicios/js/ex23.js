7ce00a9bab3e956347272a49c906de8118a39fac
vjp-yusefLC <ylaroussid01@informatica.iesvalledeljerteplasencia.es>
2025-09-19 18:30:53 +0200

    UP

--- Ejercicios/js/ex23.js
+++ Ejercicios/js/ex23.js
@@ -1,7 +1,7 @@
 let arrayNum = [2, 4, 6, 3, 2, 33];
 
 function funcionRest(...arrayNum) {
-    if ((arrayNum.every(elem => !isNaN(elem)))) {
+    if (arrayNum.every(elem => !isNaN(elem))) {
         let numMayor = Math.max(...arrayNum);
         return numMayor;
     }
