7608540fb0ff9dc5bb70367dafd07a98266695ee
vjp-yusefLC <ylaroussid01@informatica.iesvalledeljerteplasencia.es>
2025-09-25 18:00:32 +0200

    hasta ej4

--- Ejercicios/UT2/js/ejercicios.js
+++ Ejercicios/UT2/js/ejercicios.js
@@ -3,24 +3,91 @@
 
 // Ejercicio 1
 
-cadena1 = "Hola me llamo Yusef";
-cadena2 = "Hola";
-
 const Ejercicio1 = () => {
 
+    let cadena1 = "Soy Yusef";
+    let cadena2 = "Hola me llamo Yusef";
+
     function devolverMasCorta(cadena1, cadena2) {
-        if (!typeof cadena1 || !typeof cadena2) {
-            console.log("Error.");
-            return;
+
+        if (typeof cadena1 !== "string" || typeof cadena2 !== "string") {
+            return console.log("Error.");
         } else {
             if (cadena1.length < cadena2.length) {
-                return cadena1;
+                return console.log(cadena1);
             } else {
-                return cadena2;
+                return console.log(cadena2);
             }
         }
     }
-    console.log(devolverMasCorta);
+    devolverMasCorta(cadena1, cadena2);
 }
 
 // Ejercicio 2
+
+const Ejercicio2 = () => {
+
+    let cadena1 = "Arroz";
+    let cadena2 = "Zorra";
+
+    function comprobarCadenaAlReves(cadena1, cadena2) {
+        if (typeof cadena1 === "string" && typeof cadena2 === "string") {
+
+            if (cadena1.toLowerCase() === cadena2.toLowerCase().split('').reverse().join('')) {
+                console.log("La cadena 1 es igual a la 2 del reves.");
+            }
+            else {
+                console.log("La cadena 1 no es igual a la 2 del reves.");
+            }
+        }
+    }
+    comprobarCadenaAlReves(cadena1, cadena2);
+}
+
+// Ejercicio 3
+
+const Ejercicio3 = () => {
+
+    let array = [1, 2, 3, "Yusef", 4, "Hola", true];
+
+    function contarElementosArray(array) {
+        let contNum = 0;
+        let contStr = 0;
+        let contOtros = 0;
+
+        for (let elemento of array) { // Recorro el array
+            if (typeof elemento === "number") {
+                contNum++;
+            } else if (typeof elemento === "string") {
+                contStr++;
+            } else {
+                contOtros++;
+            }
+        }
+
+        console.log("Hay " + contNum + " number.");
+        console.log("Hay " + contStr + " string.");
+        console.log("Hay " + contOtros + " dato(s) que no son ni number ni string.");
+    }
+
+    contarElementosArray(array);
+}
+
+// Ejercicio 4
+
+const Ejercicio4 = () => {
+
+    let producto = "Producto generico";
+    let precio = 100;
+    let impuestos = 21;
+
+    function convertirParametros(producto, precio, impuestos) {
+        producto = String(producto);
+        parseInt(precio);
+        parseInt(impuestos);
+        console.log("El producto es " + producto + " el precio es " + precio + " y su impuesto es " + impuesto);
+    }
+
+    convertirParametros(producto, precio, impuestos);
+}
+
