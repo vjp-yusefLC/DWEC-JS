b3e15622680ba600150e0f5572a887a06891d3a9
vjp-yusefLC <ylaroussid01@informatica.iesvalledeljerteplasencia.es>
2025-09-26 17:08:01 +0200

    hasta ej8

--- Ejercicios/UT2/js/ejercicios.js
+++ Ejercicios/UT2/js/ejercicios.js
@@ -77,17 +77,107 @@ const Ejercicio3 = () => {
 
 const Ejercicio4 = () => {
 
-    let producto = "Producto generico";
-    let precio = 100;
-    let impuestos = 21;
+    let producto;
+    let precio;
+    let impuestos;
 
-    function convertirParametros(producto, precio, impuestos) {
+    function convertirParametros(producto = "Naranja", precio = 100, impuestos = null) {
         producto = String(producto);
-        parseInt(precio);
-        parseInt(impuestos);
-        console.log("El producto es " + producto + " el precio es " + precio + " y su impuesto es " + impuesto);
+        precio = parseInt(precio);
+        impuestos = parseInt(impuestos);
+        if (isNaN(precio)) {
+            precio = 100;
+        }
+        if (isNaN(impuestos)) {
+            impuestos = 21;
+        }
+        console.log("El producto es " + producto + " el precio es " + precio + " y su impuesto es " + impuestos);
     }
 
-    convertirParametros(producto, precio, impuestos);
+    convertirParametros("Naranja", true, false);
 }
 
+// Ejercicio 5
+
+const Ejercicio5 = () => {
+
+    let array = [34, 5, 2, 9, 16, 22, 7];
+
+    function ordenarArray(array) {
+        console.log("Array antes de ordenarlo: " + array + "\n");
+
+        //Utilizare el metodo de la burbuja para ordenarlo
+        for (let i = 0; i < array.length - 1; i++) {
+            for (let j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    let temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        console.log("Array una vez ordenado: " + array);
+    }
+
+    ordenarArray(array);
+}
+
+// Ejercicio 6
+
+const Ejercicio6 = () => {
+    let array = [2, 3, 4, 5];
+
+    function modificarArray(array) {
+        console.log(array.join('#'));
+
+        array.unshift(0, 1);
+        console.log(array.join('#'));
+
+        array.push(6, 7);
+        console.log(array.join('#'));
+
+        array.splice(3, 3);
+        console.log(array.join('#'));
+
+        array.splice(array.length - 1, 1, 3, 4, 7);
+        console.log(array.join('#'));
+    }
+
+    modificarArray(array);
+}
+
+// Ejercicio 7
+
+const Ejercicio7 = () => {
+    let array = [2, 4, 6, 8, 10];
+
+    function comprobarTodosValoresSonPares(array) {
+        let todosPares = array.every(elemento => {
+            if (elemento % 2 == 0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        })
+
+        if (todosPares) {
+            return "Si"
+        } else {
+            return "No"
+        }
+    }
+
+    console.log("Todos los valores del array son pares. \n¿Es cierto?: " + comprobarTodosValoresSonPares(array));
+}
+
+// EJercicio 8
+
+const Ejercicio8 = () => {
+
+    let array = [1, 2, 3, 4];
+
+    function procesarArray() {
+        
+    }
+}
